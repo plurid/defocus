b3f874f579cbbf49041e017dab82e99f05a17a3d
Jan Cavel <caveljan@gmail.com>
2020-04-19 22:23:38 +0300

    defocus-compiler > test: Compiler

--- packages/defocus-compiler/source/objects/Compiler/__tests__/index.test.ts
+++ packages/defocus-compiler/source/objects/Compiler/__tests__/index.test.ts
@@ -4,6 +4,15 @@ import Compiler from '../';
 
 describe('Compiler', () => {
     it('works', () => {
-
+        const source =
+`
+from all
+    defocus some
+`;
+        const defocusCompiler = new Compiler({
+            source,
+        });
+        const result = defocusCompiler.compile();
+        console.log(result);
     });
 });
